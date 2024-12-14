0d02502b25455382f2c7a46c89af432d4decce57
지승 <rjs8833@gmail.com>
2024-12-14 17:17:19 +0900

    test: 홍식 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<>
-			<div>yheseul test</div>
+			<div>yheseul test123</div>
 			<div className="bg-black text-white">git main test55</div>
 		</>
 	);
