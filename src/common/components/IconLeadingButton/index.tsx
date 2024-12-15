159778e69a993d8b61b971f771548decee9abd11
yheseul <kangyheseul@naver.com>
2024-12-15 20:22:18 +0900

    design: IconLeadingButton 컴포넌트 퍼블리싱 - 아이콘과 텍스트가 포함된 버튼 컴포넌트 추가 - 공용 버튼 컴포넌트와의 일관성을 유지하기 위해 아이콘 위치를 왼쪽으로 설정

---
+++ src/common/components/IconLeadingButton/index.tsx
@@ -0,0 +1,20 @@
+import { IIconLeadingButtonProps } from '@/common/types/children.interface';
+import React from 'react';
+
+const buttonPropsObject = {
+	primary: {
+		style: 'h-[52px] w-full rounded-3xl text-xl font-bold text-white flex justify-center items-center bg-primary gap-1',
+	},
+	sub: {
+		style: 'h-[52px] w-full rounded-3xl text-xl font-bold text-black flex justify-center items-center bg-lightGray gap-1',
+	},
+};
+
+export default function IconLeadingButton({ style, children, icon }: IIconLeadingButtonProps) {
+	return (
+		<button className={buttonPropsObject[style].style}>
+			<div className="text-2xl">{icon}</div>
+			{children}
+		</button>
+	);
+}
