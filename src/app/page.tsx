1489cd87b942fc51dbf7b50b5bb110a2500ab66d
지승 <rjs8833@gmail.com>
2024-12-15 21:44:48 +0900

    chore: ai 코드리뷰 테스트12

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
 	return (
 		<>
-			<div>홍홍홍식식 CD 하이루루~~</div>
-			<div className="text-whit bg-red-550 bg-black">git main test55</div>
+			<div>홍홍홍321312식식 CD 하이루루~~</div>
+			<div className="text-wh44t bg-red-550 bg-black">git main test55</div>
 		</>
 	);
 }
