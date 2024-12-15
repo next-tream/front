07d9d949b8f916739c1cc4fdc877d6d55f6914b2
yheseul <kangyheseul@naver.com>
2024-12-15 20:19:17 +0900

    design: BaseButton 공용 컴포넌트 퍼블리싱 - 공용으로 사용할 버튼 컴포넌트 퍼블리싱 완료 - 기본 스타일 및 구조 설정

---
+++ src/common/components/BaseButton/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IPrimaryButtonProps } from '@/common/types/children.interface';
+
+const buttonPropsObject = {
+	primary: {
+		style: 'flex h-[52px] w-full items-center justify-center rounded-xl text-xl font-bold text-white bg-primary',
+	},
+	sub: {
+		style: 'flex h-[52px] w-full items-center justify-center rounded-xl text-xl font-bold text-white bg-gray',
+	},
+};
+
+export default function BaseButton({ style, children }: IPrimaryButtonProps) {
+	return <button className={buttonPropsObject[style].style}>{children}</button>;
+}
