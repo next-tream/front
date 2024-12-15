aa765349f0a27f142facd23582062a11cb288bd4
yheseul <kangyheseul@naver.com>
2024-12-15 20:18:51 +0900

    chore: tailwind config 파일 경로 및 색상 추가 - content에 경로 추가 - 사용할 색상 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,9 +5,17 @@ const config: Config = {
 		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
 		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/common/**/*.{js,ts,jsx,tsx,mdx}',
 	],
 	theme: {
 		extend: {},
+		colors: {
+			primary: '#FC6B2D',
+			gray: '#6B6B6B',
+			lightGray: '#E2E4E9',
+			white: '#FFFFFF',
+			black: '#000000',
+		},
 	},
 	plugins: [],
 };
