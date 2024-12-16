8a11f04746a1089a87c3af28182879d564e6e87c
yheseul <kangyheseul@naver.com>
2024-12-16 17:39:03 +0900

    chore: 컴포넌트에서 사용하는 interface 적용 - 코드 구조 정리 및 가독성 향상

--- src/common/types/children.interface.ts
+++ src/common/types/children.interface.ts
@@ -1,3 +1,23 @@
-export interface IChildrenProps{
-    children: React.ReactNode
+export interface IChildrenProps {
+	children: React.ReactNode;
+}
+
+export interface IBaseButtonStyleProps extends IChildrenProps {
+	className: string;
+}
+
+export interface IIconLeadingButtonProps extends IBaseButtonStyleProps {
+	icon: string;
+}
+
+export interface ITheme {
+	id: string;
+	name: string;
+	icon: string;
+}
+
+export interface IThemeSelectionButtonProps {
+	theme: ITheme;
+	isSelected?: boolean;
+	onChangeThemeHandler: (id: string, checked: boolean) => void;
 }
