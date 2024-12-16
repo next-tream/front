fad94e81538c066a0fd18e6d96fdfde68d8ae20f
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-16 10:31:20 +0900

    design: 동그란 프로필 이미지 퍼블리싱

---
+++ src/common/components/CircleImage/index.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+interface ICircleImageProps {
+	src: string;
+	size?: 'm' | 'l';
+	isStreaming?: boolean;
+}
+
+export default function CircleImage({ src, size = 'm', isStreaming = false }: ICircleImageProps) {
+	const imageSize = size === 'm' ? 'w-8 h-8' : 'w-10 h-10';
+	return (
+		<div className={`rounded-full ${isStreaming ? 'border-main border-2' : ''}`}>
+			<Image
+				src={src}
+				alt={`${src}_image`}
+				width={0}
+				height={0}
+				sizes="100vw"
+				className={`${imageSize} rounded-full object-cover`}
+			/>
+		</div>
+	);
+}
