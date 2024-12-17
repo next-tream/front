d3726032bf3afd0951e7a5f09d7a43ea40b22b2e
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-17 21:10:29 +0900

    design: LogoutModal 퍼블리싱

---
+++ src/common/components/Modals/LogoutModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BaseModal from '@/common/components/Modals/BaseModal';
+
+export default function LogoutModal() {
+	return (
+		<BaseModal title="로그아웃" mainButtonLabel="확인" subButtonLabel="취소">
+			<div className="center h-full items-center">
+				<p className="text-xl font-medium text-mainBlack">로그아웃 하시겠습니까?</p>
+			</div>
+		</BaseModal>
+	);
+}
