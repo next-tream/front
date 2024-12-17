5ec276258222b2010561917cdf75e3a954bc19ce
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-17 21:15:43 +0900

    chore: 컬러명 변경

--- src/common/components/Inputs/TextInput.tsx
+++ src/common/components/Inputs/TextInput.tsx
@@ -8,13 +8,13 @@ export default function TextInput({ title, ...rest }: ITextInputProps) {
 	return (
 		<div className="flex flex-col gap-1">
 			<div className="flex items-center gap-1">
-				<p className="text-sm font-semibold text-black">{title}</p>
+				<p className="text-sm font-semibold text-mainBlack">{title}</p>
 				{/* <p className="font-medium text-xs text-error">error</p> */}
 			</div>
 			<input
 				type={rest.type}
 				placeholder={rest.placeholder}
-				className="focus:border-main h-12 w-full rounded-lg border border-[#D0D1D3] bg-[transparent] px-5 text-sm font-medium text-black outline-none"
+				className="h-12 w-full rounded-lg border border-[#D0D1D3] bg-[transparent] px-5 text-sm font-medium text-mainBlack outline-none focus:border-main"
 			/>
 		</div>
 	);
