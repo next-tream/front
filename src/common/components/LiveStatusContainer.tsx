3fb9538ca25f9661e1d7ac7c390f799157573752
yheseul <kangyheseul@naver.com>
2024-12-18 09:28:38 +0900

    feat: 라이브 시청자 수 컴포넌트 사이즈 동적 변경 기능 추가 - 사용하는 위치에 따라 시청자 수 컴포넌트의 사이즈가 변경되도록 수정 - 유연한 디자인 적용을 위해 props로 사이즈 조정 가능하게 구현

--- src/common/components/LiveStatusContainer.tsx
+++ src/common/components/LiveStatusContainer.tsx
@@ -1,16 +1,20 @@
-import Image from 'next/image';
+import { UserIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 
 interface ILiveStatusContainerProps {
+	textSize?: boolean;
 	watchingCount: number;
 }
 
-export default function LiveStatusContainer({ watchingCount }: ILiveStatusContainerProps) {
+export default function LiveStatusContainer({
+	textSize,
+	watchingCount,
+}: ILiveStatusContainerProps) {
 	return (
-		<div className="absolute left-2 top-2 flex gap-2">
+		<div className={`absolute left-2 top-2 flex gap-2 ${textSize ? 'text-xl' : 'text-sm'}`}>
 			<div className="aline-center bg-mainRed rounded-lg p-1 font-semibold">LIVE</div>
-			<div className="bg-mainBlack text-mainWhite flex gap-1 rounded-lg p-1 text-base font-semibold">
-				<Image src="/images/icon.svg" alt="icon" width={20} height={20} />
+			<div className="bg-mainBlack text-mainWhite flex items-center justify-center gap-1 rounded-lg p-1 font-semibold">
+				<UserIcon className="size-5" />
 				{watchingCount}
 			</div>
 		</div>
