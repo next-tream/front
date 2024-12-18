f3b142fac92eb1d3d1ad968b5ecd91ac41db64a2
yheseul <kangyheseul@naver.com>
2024-12-18 09:34:39 +0900

    design: main 페이지 컴포넌트 퍼블리싱 - main 페이지의 구조 및 스타일링 구현

---
+++ src/common/components/Main.tsx
@@ -0,0 +1,26 @@
+import MainVideo from '@/common/components/Video/MainVideo';
+import RecommendVideo from '@/common/components/Video/RecommendVideo';
+import CategoryContainer from './CategoryContainer';
+import MenuContainer from './MenuContainer';
+
+export default function Main() {
+	return (
+		<div className="flex h-full w-full flex-col gap-5 p-5">
+			<MainVideo />
+			<MenuContainer menu="추천" container={<RecommendVideo />} />
+			<MenuContainer
+				category={true}
+				menu="카테고리"
+				container={
+					<CategoryContainer
+						title="바람의나라"
+						image="/images/sample.jpg"
+						watchingCount={125679}
+					/>
+				}
+			/>
+			<MenuContainer menu="팔로잉" container={<RecommendVideo />} />
+			<MenuContainer menu="라이브" container={<RecommendVideo />} />
+		</div>
+	);
+}
