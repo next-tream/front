030433d8d27fd11fc34b63378fb40241c7416e36
yheseul <kangyheseul@naver.com>
2024-12-18 09:29:56 +0900

    feat: 어사이드 레이아웃 메뉴 아이템 컴포넌트에 스크롤 연동을 위한 링크 추가 - 메뉴 아이템에 링크를 추가하여 메인 페이지와 스크롤 연동 가능하도록 수정

--- src/common/components/MenuItem.tsx
+++ src/common/components/MenuItem.tsx
@@ -1,16 +1,20 @@
+import Link from 'next/link';
 import React from 'react';
 
 interface IMenuItemProps {
+	anchor: string;
 	children: React.ReactNode;
 	name: string;
 	isSelected?: boolean;
 }
 
-export default function MenuItem({ children, name, isSelected = false }: IMenuItemProps) {
+export default function MenuItem({ anchor, children, name, isSelected = false }: IMenuItemProps) {
 	return (
-		<div className="flex cursor-pointer items-center gap-3">
-			{children}
-			<p className={`text-semibold text-xl ${isSelected && 'text-main'} `}>{name}</p>
-		</div>
+		<Link href={`/#${anchor}`}>
+			<div className="flex cursor-pointer items-center gap-3">
+				{children}
+				<p className={`text-semibold text-xl ${isSelected && 'text-main'} `}>{name}</p>
+			</div>
+		</Link>
 	);
 }
