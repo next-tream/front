7fb7a4a87cefdaadb5928abbfd46709ac72f496c
yheseul <kangyheseul@naver.com>
2024-12-18 15:25:26 +0900

    design: 서치바 검색기록 드롭다운 컴포넌트 퍼블리싱 - 검색 기록을 표시하는 드롭다운 UI 추가

---
+++ src/common/components/Search/SearchHistoryList.tsx
@@ -0,0 +1,15 @@
+export default function SearchHistoryList() {
+	return (
+		<div className="bg-darkGray mt-2 h-auto w-full rounded-md p-3">
+			<ul className="flex flex-col gap-1 pb-3">
+				<li>바보 예슬</li>
+				<li>예슬콩</li>
+				<li>강예슬 회장님</li>
+			</ul>
+			<div className="border-mainWhite flex w-full justify-between border-t border-solid pt-2">
+				<button>자동저장 끄기</button>
+				<button>닫기</button>
+			</div>
+		</div>
+	);
+}
