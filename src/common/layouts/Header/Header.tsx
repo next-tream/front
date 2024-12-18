d026e0faf0c14529de596b4f58c5ea7e7d1ab5ef
yheseul <kangyheseul@naver.com>
2024-12-18 09:32:59 +0900

    feat: 헤더 스크롤 시 고정되도록 수정 - 스크롤을 내려도 헤더가 화면 상단에 고정되도록 변경

--- src/common/layouts/Header/Header.tsx
+++ src/common/layouts/Header/Header.tsx
@@ -4,7 +4,7 @@ import BaseButton from '@/common/components/Buttons/BaseButton';
 
 export default function Header() {
 	return (
-		<div className="border-darkGray flex flex-row items-center justify-between border-b border-solid pb-5">
+		<div className="border-darkGray bg-mainBlack sticky top-0 z-50 flex flex-row items-center justify-between border-b border-solid py-5">
 			<div className="flex flex-row items-center justify-between">
 				<Bars3Icon className="size-14" />
 				<p className="text-5xl font-semibold">Nextream</p>
