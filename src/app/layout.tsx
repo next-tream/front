2c2bfe83cca8bb5a2c9a4e1e8f32a3fbd96d0e11
yheseul <kangyheseul@naver.com>
2024-12-18 11:19:27 +0900

    style: Header에 정의한 padding-x 스타일을 body에 적용 - Header에서 사용한 padding-x 스타일을 body에도 적용하여 일관된 레이아웃 유지

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { IChildrenProps } from '@/common/types/children.interface';
 export default function RootLayout({ children }: IChildrenProps) {
 	return (
 		<html lang="ko">
-			<body className="bg-mainBlack text-mainWhite m-0 box-border h-screen w-screen">
+			<body className="bg-mainBlack text-mainWhite m-0 box-border h-screen w-screen px-5">
 				<Layout>{children}</Layout>
 			</body>
 		</html>
