266797c3b435bf3308657085be4c514ca7837041
yheseul <kangyheseul@naver.com>
2024-12-18 18:29:46 +0900

    feat: 검색 기록 로컬스토리지에 저장하고 드롭다운 리스트에 표시 - 검색어를 로컬스토리지에 저장하여 검색 기록 드롭다운 리스트에 표시되도록 구현

--- src/common/components/Search/SearchWrapper.tsx
+++ src/common/components/Search/SearchWrapper.tsx
@@ -5,11 +5,16 @@ import SearchBar from './SearchBar';
 import SearchHistoryList from './SearchHistoryList';
 
 export default function SearchWrapper() {
-	const { inputText, onChangeInput, onClickReset } = useInputText();
+	const { inputText, onChangeInput, onClickReset, onClickSearch } = useInputText();
 
 	return (
-		<div className={`w-3/12 ${inputText && 'mt-40'}`}>
-			<SearchBar value={inputText} onChange={onChangeInput} onClick={onClickReset} />
+		<div className="relative w-3/12">
+			<SearchBar
+				inputText={inputText}
+				onChangeInput={onChangeInput}
+				onClickReset={onClickReset}
+				onClickSearch={onClickSearch}
+			/>
 			{inputText && <SearchHistoryList />}
 		</div>
 	);
--- src/common/components/Search/hooks/useInputText.ts
+++ src/common/components/Search/hooks/useInputText.ts
@@ -1,10 +1,11 @@
 'use client';
 
 import { useState } from 'react';
-import { TInputChangeEvent } from '@/common/types/children.interface';
+import { TInputChangeEvent } from '@/common/types/handler.type';
 
 export const useInputText = () => {
 	const [inputText, setInputText] = useState('');
+	let searchHistoryValues = [];
 
 	const onChangeInput = (event: TInputChangeEvent) => {
 		setInputText(event.target.value);
@@ -14,9 +15,19 @@ export const useInputText = () => {
 		setInputText('');
 	};
 
+	const onClickSearch = () => {
+		const searchHistoryList = localStorage.getItem('search');
+		searchHistoryList && searchHistoryValues.push(...JSON.parse(searchHistoryList));
+		searchHistoryValues.push(inputText);
+		localStorage.removeItem('search');
+		localStorage.setItem('search', JSON.stringify(searchHistoryValues));
+		setInputText('');
+	};
+
 	return {
 		inputText,
 		onChangeInput,
 		onClickReset,
+		onClickSearch,
 	};
 };
