ac0e68a216f9084ef074a632b03c63d870b9d564
yheseul <kangyheseul@naver.com>
2024-12-18 11:38:13 +0900

    style: aspect-video 사용하여 영상 썸네일 비율 수정 - aspect-video 클래스를 사용하여 영상 썸네일의 비율을 적절하게 수정

--- src/common/components/MenuContainer.tsx
+++ src/common/components/MenuContainer.tsx
@@ -1,20 +1,10 @@
-export default function MenuContainer({
-	category = false,
-	menu,
-	container,
-}: {
-	category?: boolean;
-	menu: string;
-	container: any;
-}) {
+export default function MenuContainer({ menu, container }: { menu: string; container: any }) {
 	return (
-		<div
-			className={`flex h-${category ? '[300px]' : '[500px]'} w-full flex-col gap-3 text-3xl font-semibold`}
-		>
+		<div className="flex aspect-video flex-col gap-3 text-3xl font-semibold">
 			<div>{menu}</div>
-			<div className={`grid h-full w-full grid-cols-${category ? '6' : '3'} gap-3`}>
-				{new Array(6).fill(0).map(() => (
-					<div>{container} </div>
+			<div className="grid h-full w-full grid-cols-3 gap-3">
+				{new Array(6).fill(0).map((_, index) => (
+					<div key={index}>{container}</div>
 				))}
 			</div>
 		</div>
--- src/common/components/Video/MainVideo.tsx
+++ src/common/components/Video/MainVideo.tsx
@@ -3,7 +3,7 @@ import LiveStatusContainer from '@/common/components/LiveStatusContainer';
 
 export default function MainVideo() {
 	return (
-		<div className="relative h-[500px] w-full rounded-2xl">
+		<div className="relative aspect-video rounded-2xl">
 			<Image src="/images/main.png" alt="main" fill />
 			<div className="absolute left-3 top-3 flex">
 				<LiveStatusContainer textSize={true} watchingCount={986} />
