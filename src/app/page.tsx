4552fae78aaf59206af0d64939dfd57b1f847b33
yheseul <kangyheseul@naver.com>
2024-12-18 10:23:32 +0900

    refactor: 메인 컴포넌트 삭제 및 코드 통합 - 메인 컴포넌트를 삭제하고, 기존 코드들을 메인 페이지에 직접 통합 - 사이트 방문 시 첫 메인 화면이 바로 보이도록 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
+import CategoryContainer from '@/common/components/CategoryContainer';
+import MenuContainer from '@/common/components/MenuContainer';
+import MainVideo from '@/common/components/Video/MainVideo';
+import RecommendVideo from '@/common/components/Video/RecommendVideo';
+
 export default function Home() {
 	return (
-		<>
-			<div>홍홍홍32131555식식 CD 하이루루~~</div>
-			<div className="text-wh44t bg-red-550 bg-black">git main test55</div>
-		</>
+		<div className="flex h-full w-full flex-col gap-5 p-5">
+			<MainVideo />
+			<MenuContainer menu="추천" container={<RecommendVideo />} />
+			<MenuContainer
+				category={true}
+				menu="카테고리"
+				container={
+					<CategoryContainer
+						title="바람의나라"
+						image="/images/sample.jpg"
+						watchingCount={125679}
+					/>
+				}
+			/>
+			<MenuContainer menu="팔로잉" container={<RecommendVideo />} />
+			<MenuContainer menu="라이브" container={<RecommendVideo />} />
+		</div>
 	);
 }
