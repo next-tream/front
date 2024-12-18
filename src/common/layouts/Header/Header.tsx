9d3cee08a05aa186c2014c60adddaeac0824fb4c
yheseul <kangyheseul@naver.com>
2024-12-18 15:29:14 +0900

    feat: 서치바와 검색기록 드롭다운 wrapper 컴포넌트를 헤더에 적용 - wrapper 컴포넌트를 헤더에 통합 적용

--- src/common/layouts/Header/Header.tsx
+++ src/common/layouts/Header/Header.tsx
@@ -1,15 +1,16 @@
 import { Bars3Icon } from '@heroicons/react/24/solid';
-import SearchBar from '@/common/components/SearchBar';
 import BaseButton from '@/common/components/Buttons/BaseButton';
+import { useInputText } from '@/common/components/Search/hooks/useInputText';
+import SearchWrapper from '@/common/components/Search/SearchWrapper';
 
 export default function Header() {
 	return (
-		<div className="border-darkGray bg-mainBlack sticky top-0 z-50 flex flex-row items-center justify-between border-b border-solid py-5">
+		<div className="border-darkGray bg-mainBlack sticky top-0 z-50 flex h-24 flex-row items-center justify-between border-b border-solid py-5">
 			<div className="flex items-center justify-between">
 				<Bars3Icon className="size-14" />
 				<p className="text-5xl font-semibold">Nextream</p>
 			</div>
-			<SearchBar />
+			<SearchWrapper />
 			<div className="w-24">
 				<BaseButton className="basePrimaryBtn">로그인</BaseButton>
 			</div>
