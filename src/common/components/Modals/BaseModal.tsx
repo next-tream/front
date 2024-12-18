f8f9166d0071c5e948c27b3ed15fbaeefb2048f0
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-18 18:18:49 +0900

    design: 모달 컴포넌트 중앙 정렬 및 배경 추가

--- src/common/components/Modals/BaseModal.tsx
+++ src/common/components/Modals/BaseModal.tsx
@@ -14,32 +14,37 @@ export default function BaseModal({
 	const { title, description, mainButtonLabel, subButtonLabel } = MODAL_CONFIG[type];
 
 	return (
-		<div className="relative flex h-[544px] max-h-full w-1/3 max-w-md flex-col gap-8 rounded-2xl bg-mainWhite px-6 pb-4 pt-11">
-			{showCloseButton && <CloseButton />}
-			<div className="center flex-col gap-7">
-				<div className="text-4xl font-bold text-mainBlack">{title}</div>
+		<>
+			<div className="fixed inset-0 z-40 bg-mainBlack bg-opacity-50" />
+			<div className="center fixed inset-0 z-50">
+				<div className="relative flex h-1/2 min-h-[544px] w-1/3 max-w-md flex-col gap-8 rounded-2xl bg-mainWhite px-6 pb-4 pt-11">
+					{showCloseButton && <CloseButton />}
+					<div className="center flex-col gap-7">
+						<div className="text-4xl font-bold text-mainBlack">{title}</div>
 
-				{description && (
-					<div className="center flex-col font-semibold text-mainBlack">
-						{splitByDotAndRenderParagraphs(description)}
+						{description && (
+							<div className="center flex-col font-semibold text-mainBlack">
+								{splitByDotAndRenderParagraphs(description)}
+							</div>
+						)}
 					</div>
-				)}
-			</div>
 
-			<div className="h-full">{children}</div>
+					<div className="h-full">{children}</div>
 
-			<div className="flex gap-5">
-				{subButtonLabel && (
-					<BaseButton className="baseSubBtn" onClick={onSubButtonClick}>
-						{subButtonLabel}
-					</BaseButton>
-				)}
-				{mainButtonLabel && (
-					<BaseButton className="basePrimaryBtn" onClick={onMainButtonClick}>
-						{mainButtonLabel}
-					</BaseButton>
-				)}
+					<div className="flex gap-5">
+						{subButtonLabel && (
+							<BaseButton className="baseSubBtn" onClick={onSubButtonClick}>
+								{subButtonLabel}
+							</BaseButton>
+						)}
+						{mainButtonLabel && (
+							<BaseButton className="basePrimaryBtn" onClick={onMainButtonClick}>
+								{mainButtonLabel}
+							</BaseButton>
+						)}
+					</div>
+				</div>
 			</div>
-		</div>
+		</>
 	);
 }
