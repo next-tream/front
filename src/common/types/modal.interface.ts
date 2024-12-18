2e854569247bea9441b7f7d9673413966aa44244
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-18 16:28:36 +0900

    feat: TextInputsWrapper 컴포넌트 생성 및 모달 관련 전역 상수 타입 정의 - TextInputsWrapper 컴포넌트 구현 후 전역 상수 MODAL_CONFIG 를 기반으로 렌더링 - 전역 상수에 대한 타입 정의 및 검증

--- src/common/types/modal.interface.ts
+++ src/common/types/modal.interface.ts
@@ -1,8 +1,21 @@
 import { IChildrenProps } from '@/common/types/children.interface';
-import MODAL_CONSTANTS from '@/common/constants/modal.const';
+import { MODAL_CONSTANTS } from '@/common/constants/modal.const';
+import { ITextInput } from './textInput.interface';
+
+export type ModalKey = keyof typeof MODAL_CONSTANTS;
+
+export interface IModalConfig {
+	readonly title: string;
+	readonly description: string;
+	readonly mainButtonLabel: string;
+	readonly subButtonLabel: string;
+	readonly inputs: readonly ITextInput[];
+}
+
+export type TModal = Record<ModalKey, IModalConfig>;
 
 export interface IBaseModalProps extends IChildrenProps {
-	type: keyof typeof MODAL_CONSTANTS;
+	type: ModalKey;
 	showCloseButton?: boolean;
 	onMainButtonClick?: () => void;
 	onSubButtonClick?: () => void;
