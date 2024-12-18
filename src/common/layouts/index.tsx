6287aa9f345fc132c922cf4afcc2879d8ef5a20c
yheseul <kangyheseul@naver.com>
2024-12-18 11:20:42 +0900

    style: 스타일 변경

--- src/common/layouts/index.tsx
+++ src/common/layouts/index.tsx
@@ -4,12 +4,12 @@ import Header from '@/common/layouts/Header/Header';
 
 export default function Layout({ children }: IChildrenProps) {
 	return (
-		<div className="px-5">
+		<>
 			<Header />
 			<div className="flex flex-row">
 				<Aside />
 				<div className="w-full">{children}</div>
 			</div>
-		</div>
+		</>
 	);
 }
