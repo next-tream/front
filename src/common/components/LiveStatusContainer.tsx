2e75ac314ff0a878ac57f6df9f4f0ee00d86561b
yheseul <kangyheseul@naver.com>
2024-12-18 10:01:57 +0900

    chore: '.center' 클래스를 'center'로 변경 - 테일윈드에 정의된 '.center' 클래스를 'center'로 변경하여 코드 간결화

--- src/common/components/LiveStatusContainer.tsx
+++ src/common/components/LiveStatusContainer.tsx
@@ -13,7 +13,7 @@ export default function LiveStatusContainer({
 	return (
 		<div className={`absolute left-2 top-2 flex gap-2 ${textSize ? 'text-xl' : 'text-sm'}`}>
 			<div className="aline-center bg-mainRed rounded-lg p-1 font-semibold">LIVE</div>
-			<div className="bg-mainBlack text-mainWhite flex items-center justify-center gap-1 rounded-lg p-1 font-semibold">
+			<div className="bg-mainBlack text-mainWhite center gap-1 rounded-lg p-1 font-semibold">
 				<UserIcon className="size-5" />
 				{watchingCount}
 			</div>
