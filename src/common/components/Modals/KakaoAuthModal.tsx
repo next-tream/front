58fc4eb7d1704955a5323e14c8a3515fafc31f74
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-20 07:55:34 +0900

    refactor: OTP Input 컴포넌트 분리 및 적용

--- src/common/components/Modals/KakaoAuthModal.tsx
+++ src/common/components/Modals/KakaoAuthModal.tsx
@@ -1,8 +1,8 @@
 import BaseModal from '@/common/components/Modals/BaseModal';
 import { IKakaoAuthModalProps } from '@/common/types/modal.interface';
-import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import Timer from '@/common/components/Timer';
+import OtpInput from '@/common/components/Inputs/OtpInput';
 
 export default function KakaoAuthModal({ authenticationTime }: IKakaoAuthModalProps) {
 	return (
@@ -11,16 +11,9 @@ export default function KakaoAuthModal({ authenticationTime }: IKakaoAuthModalPr
 				<div className="flexCol items-end gap-4">
 					<div className="center relative w-full">
 						<Timer time={authenticationTime} />
-						<ArrowPathIcon className="text-mainBlack absolute right-2 top-0 size-4" />
+						<ArrowPathIcon className="absolute right-2 top-0 size-4 text-mainBlack" />
 					</div>
-
-					<InputOTP maxLength={6}>
-						<InputOTPGroup>
-							{new Array(6).fill('').map((_, index) => (
-								<InputOTPSlot key={index} index={index} className="size-14" />
-							))}
-						</InputOTPGroup>
-					</InputOTP>
+					<OtpInput />
 				</div>
 			</div>
 		</BaseModal>
