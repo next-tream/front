75944b32cd0cad6197a3cc70f79c6235057af831
yheseul <kangyheseul@naver.com>
2024-12-19 18:51:03 +0900

    feat: aside layout에서 분리된 컴포넌트를 레이아웃에 적용하여 유지보수성 개선

--- src/common/layouts/Aside/index.tsx
+++ src/common/layouts/Aside/index.tsx
@@ -1,34 +1,14 @@
-'use client';
-
-import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
-
 import Divider from '@/common/components/Divider';
-import MenuItemWrapper from '@/common/components/MenuItems/MenuItemWrapper';
-import StreamerInfoContainerWrapper from '@/common/components/StreamerInfoContainers/StreamerInfoContainerWrapper';
-import { mainMenuItems } from '@/common/constants/menuItems.const';
-import { useToggle } from '@/common/hooks/useToggle';
+import AsideMenu from '@/common/components/Layouts/AsideMenu';
+import AsideRecommendChannel from '@/common/components/Layouts/AsideRecommendChannel';
 
 export default function Aside() {
-	const { isToggle, onClickToggle } = useToggle(true);
-
 	return (
 		<div className="border-darkGray relative flex min-h-[90dvh] w-1/4 flex-col border-r border-solid py-5 pl-2 pr-3">
 			<div className="fixed flex w-[18%] flex-col gap-4">
-				<MenuItemWrapper items={mainMenuItems} />
+				<AsideMenu />
 				<Divider color="lightGray" />
-				<div>
-					<div className="betweenCenter flex-row text-xl">
-						<p>추천 채널</p>
-						<button onClick={onClickToggle}>
-							{isToggle ? (
-								<ChevronUpIcon className="size-6" />
-							) : (
-								<ChevronDownIcon className="size-6" />
-							)}
-						</button>
-					</div>
-					{isToggle && <StreamerInfoContainerWrapper />}
-				</div>
+				<AsideRecommendChannel />
 			</div>
 		</div>
 	);
