69d10f174aa03fc5fed7b6b52151808637511eb4
yheseul <kangyheseul@naver.com>
2024-12-19 09:32:52 +0900

    feat: anchor prop 유무에 따라 gap 변경 - anchor prop이 존재하면 gap 값을 변경하도록 수정 - 레이아웃 구성에 유연성 추가

--- src/common/components/MenuItems/MenuItemWrapper.tsx
+++ src/common/components/MenuItems/MenuItemWrapper.tsx
@@ -1,10 +1,10 @@
-import { mainMenuItems } from '@/common/constants/menuItems.const';
 import MenuItem from '@/common/components/MenuItems/MenuItem';
+import { IMenuItemWrapperProps } from '@/common/types/object.interface';
 
-export default function MenuItemWrapper() {
+export default function MenuItemWrapper({ items }: { items: IMenuItemWrapperProps[] }) {
 	return (
-		<div className="border-darkGray flex flex-col gap-6 border-b border-solid pb-5">
-			{mainMenuItems.map((item) => (
+		<div className={`border-darkGray flex flex-col gap-${items[0].anchor ? '6' : '1'}`}>
+			{items.map((item) => (
 				<MenuItem anchor={item.anchor} key={item.name} name={item.name}>
 					{item.icon}
 				</MenuItem>
