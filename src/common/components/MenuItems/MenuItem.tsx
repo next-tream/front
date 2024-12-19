8a474a86c184922a85fb89d4df74d6d9b59a83db
yheseul <kangyheseul@naver.com>
2024-12-19 09:31:20 +0900

    feat: 메뉴 호버 시 텍스트 색상을 main 컬러로 변경 - 메뉴 항목에 호버 효과를 추가하여 텍스트 색상이 main 컬러로 변경 - 사용자 인터페이스 개선

--- src/common/components/MenuItems/MenuItem.tsx
+++ src/common/components/MenuItems/MenuItem.tsx
@@ -1,18 +1,16 @@
 import Link from 'next/link';
-
-interface IMenuItemProps {
-	anchor: string;
-	children: React.ReactNode;
-	name: string;
-	isSelected?: boolean;
-}
+import { IMenuItemProps } from '@/common/types/menuItem.interface';
 
 export default function MenuItem({ anchor, children, name, isSelected = false }: IMenuItemProps) {
 	return (
 		<Link href={`/#${anchor}`}>
-			<div className="flex cursor-pointer items-center gap-3">
+			<div className="hover:text-main flex cursor-pointer items-center gap-3">
 				{children}
-				<p className={`text-semibold text-xl ${isSelected && 'text-main'} `}>{name}</p>
+				<p
+					className={`text-semibold text-${anchor ? 'xl' : 'base'} ${isSelected && 'text-main'}`}
+				>
+					{name}
+				</p>
 			</div>
 		</Link>
 	);
