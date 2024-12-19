863134fed808058d91baf6330aee6aa16bc23470
yheseul <kangyheseul@naver.com>
2024-12-19 17:50:14 +0900

    feat: 프로필 정보 컴포넌트에서 사용할 타입 정의

--- src/common/types/profile.interface.ts
+++ src/common/types/profile.interface.ts
@@ -3,3 +3,8 @@ import { IChildrenProps } from './children.interface';
 export interface IProfileSubMenuProps extends IChildrenProps {
 	color: string;
 }
+
+export interface IProfileContainerProps {
+	nickName: string;
+	introduceYourself: string;
+}
