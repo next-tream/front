60b68a0fe0c77bc10556c4019e80eaa55a5b021c
yheseul <kangyheseul@naver.com>
2024-12-19 18:36:06 +0900

    refactor: 추천 채널 컴포넌트 분리 - 로그인 상태 및 유저에 따라 동적으로 변경되는 추천 채널을 별도의 컴포넌트로 분리 - 리렌더를 최소화하여 성능 최적화

---
+++ src/common/components/Layout/AsideRecommendChannel.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useToggle } from '@/common/hooks/useToggle';
+import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
+import StreamerInfoContainerWrapper from '../StreamerInfoContainers/StreamerInfoContainerWrapper';
+
+export default function AsideRecommendChannel() {
+	const { isToggle, onClickToggle } = useToggle(true);
+
+	return (
+		<div>
+			<div className="betweenCenter flex-row text-xl">
+				<p>추천 채널</p>
+				<button onClick={onClickToggle}>
+					{isToggle ? (
+						<ChevronUpIcon className="size-6" />
+					) : (
+						<ChevronDownIcon className="size-6" />
+					)}
+				</button>
+			</div>
+			{isToggle && <StreamerInfoContainerWrapper />}
+		</div>
+	);
+}
