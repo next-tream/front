c7308f360884ed15a6ec36d3871128c66f95f14f
yheseul <kangyheseul@naver.com>
2024-12-19 09:26:12 +0900

    feat: 헤더 레이아웃에 패딩 추가 및 로그인 시 프로필 UI 적용 - 헤더 레이아웃에 패딩을 추가하여 디자인 개선 - 로그인 상태에 따라 프로필 UI를 표시하도록 구현

--- src/common/layouts/Header/Header.tsx
+++ src/common/layouts/Header/Header.tsx
@@ -1,18 +1,20 @@
 import { Bars3Icon } from '@heroicons/react/24/solid';
-import BaseButton from '@/common/components/Buttons/BaseButton';
+// import BaseButton from '@/common/components/Buttons/BaseButton';
 import SearchWrapper from '@/common/components/Search/SearchWrapper';
+import UserState from '@/common/components/UserState';
 
 export default function Header() {
 	return (
-		<div className="border-darkGray bg-mainBlack betweenCenter sticky top-0 z-50 h-24 flex-row border-b border-solid py-5">
+		<div className="border-darkGray bg-mainBlack betweenCenter sticky top-0 z-50 h-24 flex-row border-b border-solid py-5 pr-3.5">
 			<div className="between">
 				<Bars3Icon className="size-14" />
 				<p className="text-5xl font-semibold">Nextream</p>
 			</div>
 			<SearchWrapper />
 			<div className="w-24">
-				<BaseButton className="basePrimaryBtn">로그인</BaseButton>
+				{/* <BaseButton className="basePrimaryBtn">로그인</BaseButton> */}
 			</div>
+			<UserState />
 		</div>
 	);
 }
