d138b4e6a499ad032bba3df141382dd167dd54fa
yheseul <kangyheseul@naver.com>
2024-12-19 09:33:45 +0900

    style: 스타일 수정 및 Divider 컴포넌트 적용 - 레이아웃에 필요한 스타일 수정 - Divider 컴포넌트 적용하여 시각적 구분 추가

--- src/common/layouts/Aside/index.tsx
+++ src/common/layouts/Aside/index.tsx
@@ -4,16 +4,19 @@ import { useToggle } from '@/common/hooks/useToggle';
 import MenuItemWrapper from '@/common/components/MenuItems/MenuItemWrapper';
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
 import StreamerInfoContainerWrapper from '@/common/components/StreamerInfoContainers/StreamerInfoContainerWrapper';
+import { mainMenuItems } from '@/common/constants/menuItems.const';
+import Divider from '@/common/components/Divider';
 
 export default function Aside() {
-	const { isToggle, onClickToggle } = useToggle();
+	const { isToggle, onClickToggle } = useToggle(true);
 
 	return (
 		<div className="border-darkGray relative flex min-h-[90dvh] w-1/4 flex-col border-r border-solid py-5 pl-2 pr-3">
-			<div className="fixed w-[18%]">
-				<MenuItemWrapper />
+			<div className="fixed flex w-[18%] flex-col gap-4">
+				<MenuItemWrapper items={mainMenuItems} />
+				<Divider color="lightGray" />
 				<div>
-					<div className="betweenCenter flex-row pt-3 text-xl">
+					<div className="betweenCenter flex-row text-xl">
 						<p>추천 채널</p>
 						<div onClick={onClickToggle}>
 							{isToggle ? (
