d855e6a0f4be9cdcec439849ee3c8b14029f7220
yheseul <kangyheseul@naver.com>
2024-12-19 18:41:02 +0900

    refactor: 동적 메뉴 항목 분리를 위한 컴포넌트 분리 - 페이지에 따라 동적으로 변경되는 메뉴 항목들을 별도의 컴포넌트로 분리 - 코드의 가독성 및 유지보수성 향상

---
+++ src/common/components/Layouts/AsideMenu.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import MenuItemWrapper from '../MenuItems/MenuItemWrapper';
+import { mainMenuItems, userMenuItems } from '@/common/constants/menuItems.const';
+
+export default function AsideMenu() {
+	const path = usePathname();
+	return (
+		<div>
+			{path === '/' && <MenuItemWrapper items={mainMenuItems} isAside={true} />}
+			{path === '/profile/myProfile' && (
+				<MenuItemWrapper items={userMenuItems} isAside={true} />
+			)}
+		</div>
+	);
+}
