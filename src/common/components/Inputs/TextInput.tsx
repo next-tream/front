83feeb4c0659014b71ba1497b152a2001b4637a1
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-20 07:55:12 +0900

    refactor: 타입 파일 분리 및 적용

--- src/common/components/Inputs/TextInput.tsx
+++ src/common/components/Inputs/TextInput.tsx
@@ -1,20 +1,16 @@
-import { InputHTMLAttributes } from 'react';
-
-interface ITextInputProps extends InputHTMLAttributes<HTMLInputElement> {
-	title?: string;
-}
+import { ITextInputProps } from '@/common/types/input.interface';
 
 export default function TextInput({ title, ...rest }: ITextInputProps) {
 	return (
 		<div className="flexCol gap-1">
 			<div className="flex items-center gap-1">
-				<p className="text-mainBlack text-sm font-semibold">{title}</p>
+				<p className="text-sm font-semibold text-mainBlack">{title}</p>
 				{/* <p className="font-medium text-xs text-error">error</p> */}
 			</div>
 			<input
 				type={rest.type}
 				placeholder={rest.placeholder}
-				className="border-lightGray text-mainBlack focus:border-main h-12 w-full rounded-lg border bg-[transparent] px-5 text-sm font-medium outline-none"
+				className="h-12 w-full rounded-lg border border-lightGray bg-[transparent] px-5 text-sm font-medium text-mainBlack outline-none focus:border-main"
 			/>
 		</div>
 	);
