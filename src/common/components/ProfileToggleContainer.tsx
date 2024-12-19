353d8d8d88cbc44e125683180426789e9caad6b8
yheseul <kangyheseul@naver.com>
2024-12-19 18:46:50 +0900

    feat: 메뉴 아이템 컴포넌트에서 크기 조정을 위해 조건부 프롭스 추가 - 크기 변경이 필요한 경우에만 해당 프롭스를 통해 다르게 적용

--- src/common/components/ProfileToggleContainer.tsx
+++ src/common/components/ProfileToggleContainer.tsx
@@ -6,7 +6,7 @@ export default function ProfileToggleContainer() {
 	return (
 		<div className="bg-subBlack border-main absolute right-px mt-4 flex w-48 flex-col gap-2 rounded-2xl border px-5 py-3.5">
 			<StreamerInfoContainer name="치즈루" category="Lv.99" />
-			<MenuItemWrapper items={userMenuItems} />
+			<MenuItemWrapper items={userMenuItems} isAside={false} />
 		</div>
 	);
 }
