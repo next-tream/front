b3ee1599e307c92a55ae570698a57f4f809c0340
yheseul <kangyheseul@naver.com>
2024-12-19 13:24:45 +0900

    chore: 의미 없는 코드 삭제

--- src/common/components/ProfileToggleContainer.tsx
+++ src/common/components/ProfileToggleContainer.tsx
@@ -6,9 +6,7 @@ export default function ProfileToggleContainer() {
 	return (
 		<div className="bg-subBlack border-main absolute right-px mt-4 flex w-48 flex-col gap-2 rounded-2xl border px-5 py-3.5">
 			<StreamerInfoContainer name="치즈루" category="Lv.99" />
-			<div>
-				<MenuItemWrapper items={userMenuItems} />
-			</div>
+			<MenuItemWrapper items={userMenuItems} />
 		</div>
 	);
 }
