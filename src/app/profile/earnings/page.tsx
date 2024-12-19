9f0910c296fff64b00922744e40b7c17f2480c46
yheseul <kangyheseul@naver.com>
2024-12-20 08:19:12 +0900

    design: 이번달 수익 페이지 퍼블리싱 - 이번달 수익 정보를 보여주는 페이지 레이아웃 디자인 완료 - 반응형 스타일 적용

---
+++ src/app/profile/earnings/page.tsx
@@ -0,0 +1,17 @@
+import Earnings from '@/common/components/Earnings';
+import PageTitle from '@/common/components/PageTitle';
+
+export default function EarningsPage() {
+	return (
+		<div className="flexCol gap-8">
+			<PageTitle title="수입 대시보드" />
+			<div className="border-mainWhite flexCol gap-4 rounded-md border lg:p-9 2xl:p-12">
+				<p className="font-medium lg:text-2xl 2xl:text-3xl">총 수입</p>
+				<div className="flexCol gap-1">
+					<Earnings earnings={180000} />
+					<p className="text-darkGray lg:text-lg 2xl:text-2xl">이번 달</p>
+				</div>
+			</div>
+		</div>
+	);
+}
