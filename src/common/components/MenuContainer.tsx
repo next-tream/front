644c425288183982f222cfae20b9f325d3b85b94
yheseul <kangyheseul@naver.com>
2024-12-19 17:46:00 +0900

    feat: 페이지 공통 타이틀 적용 - 여러 페이지에서 재사용 가능한 공통 타이틀 컴포넌트 추가

--- src/common/components/MenuContainer.tsx
+++ src/common/components/MenuContainer.tsx
@@ -1,9 +1,10 @@
-import { IMenuContainerProps } from '../types/container.interface';
+import PageTitle from './PageTitle';
+import { IMenuContainerProps } from '@/common/types/container.interface';
 
 export default function MenuContainer({ menu, children }: IMenuContainerProps) {
 	return (
 		<div className="flex aspect-video flex-col gap-3 text-3xl font-semibold">
-			<div>{menu}</div>
+			<PageTitle title={menu} />
 			<div className="grid h-full w-full grid-cols-3 gap-3">
 				{new Array(6).fill(0).map((_, index) => (
 					<div key={index}>{children}</div>
