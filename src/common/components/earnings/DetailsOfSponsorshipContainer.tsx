06e0cc5bfd035d6bddda5c0842a9aa1c95bf2487
yheseul <kangyheseul@naver.com>
2024-12-20 09:07:18 +0900

    design: 후원 내역 컴포넌트 퍼블리싱 - 후원 내역을 표시하는 컴포넌트의 레이아웃 및 스타일 적용 - 주요 정보 및 아이콘 배치 완료 - 반응형 스타일 적용

---
+++ src/common/components/earnings/DetailsOfSponsorshipContainer.tsx
@@ -0,0 +1,20 @@
+import Divider from '@/common/components/Divider';
+import PageTitle from '@/common/components/PageTitle';
+import SponsorshipItemList from './SponsorshipItemList';
+
+export default function DetailsOfSponsorshipContainer() {
+	return (
+		<div className="border-mainWhite flexCol gap-6 rounded-md border lg:p-7 2xl:p-9">
+			<PageTitle title="후원 내역" />
+			<div className="border-mainWhite flexCol gap-2 rounded-md border lg:p-5 2xl:p-7">
+				<div className="between px-9 py-4 lg:text-base 2xl:text-xl">
+					<p>날짜</p>
+					<p>후원자</p>
+					<p>금액</p>
+				</div>
+				<Divider color="mainWhite" />
+				<SponsorshipItemList />
+			</div>
+		</div>
+	);
+}
