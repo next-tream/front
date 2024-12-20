cda4723a65b6fcf59ad0967511a4eb361726a481
yheseul <kangyheseul@naver.com>
2024-12-20 13:04:41 +0900

    feat: 이번 달 수익 페이지에 분리된 월별 수익 차트 컴포넌트 페이지에 통합

--- src/app/profile/earnings/page.tsx
+++ src/app/profile/earnings/page.tsx
@@ -1,12 +1,14 @@
 import PageTitle from '@/common/components/PageTitle';
 import TotalIncome from '@/common/components/earnings/TotalIncome';
+import ChartContainer from '@/common/components/earnings/ChartContainer';
 import DetailsOfSponsorshipContainer from '@/common/components/earnings/DetailsOfSponsorshipContainer';
 
 export default function EarningsPage() {
 	return (
-		<div className="flexCol w-full gap-8">
+		<div className="flexCol h-full w-full gap-8">
 			<PageTitle title="수입 대시보드" />
 			<TotalIncome />
+			<ChartContainer />
 			<DetailsOfSponsorshipContainer />
 		</div>
 	);
