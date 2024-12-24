4db0dcb4ed13313d2e67c820b424e43654186488
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-24 19:18:49 +0900

    feat: 레이아웃 타입 구현

--- src/common/types/children.interface.ts
+++ src/common/types/children.interface.ts
@@ -1,3 +1,10 @@
+import { Session } from 'next-auth';
+
 export interface IChildrenProps {
 	children: React.ReactNode;
+}
+
+export interface ILayoutProps extends IChildrenProps {
+	session: Session;
+	modal?: React.ReactNode;
 }
