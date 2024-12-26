43d69ee1c98de193eb5ac560e5d5b9580958f59f
yheseul <kangyheseul@naver.com>
2024-12-26 23:59:11 +0900

    feat: 회원가입 유효성 검사 관련 타입 추가 생성
    - 유효성 검사 로직에 필요한 타입 정의 추가
    - 입력 필드 및 에러 메시지 처리에 활용될 타입 설계

--- src/common/types/textInput.interface.ts
+++ src/common/types/textInput.interface.ts
@@ -1,12 +1,15 @@
 import { HTMLInputTypeAttribute } from 'react';
 import { ModalKey } from './modal.interface';
+import { IValidationErrors } from './input.interface';
 
 export interface ITextInputsWrapperProps {
 	type: ModalKey;
+	errors?: IValidationErrors;
 }
 
 export interface ITextInput {
 	title?: string;
 	placeholder?: string;
 	type: HTMLInputTypeAttribute;
+	name?: string;
 }
