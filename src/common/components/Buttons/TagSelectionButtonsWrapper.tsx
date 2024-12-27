4a8edf76eaa1ba7f961320db9bdf8b1df468e003
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-27 10:57:58 +0900

    refactor: 테마 선택 모달에서 선택된 태그 값 필요로 인해 Props 끌어올려 전달하도록 수정

--- src/common/components/Buttons/TagSelectionButtonsWrapper.tsx
+++ src/common/components/Buttons/TagSelectionButtonsWrapper.tsx
@@ -1,12 +1,19 @@
 'use client';
 
 import { tags } from '@/common/constants/themeSelectionButton.const';
-import useTagSelectionButton from '@/common/hooks/useThemeSelectionButton';
 import TagSelectionButton from '@/common/components/Buttons/TagSelectionButton';
 
-export default function TagSelectionButtonsWrapper({ isTag = false }: { isTag?: boolean }) {
-	const { onChangeTagHandler, selectedTags } = useTagSelectionButton();
+interface ITagSelectionButtonsWrapperProps {
+	isTag?: boolean;
+	selectedTags: number[];
+	onChangeTagHandler: (id: number, checked: boolean) => void;
+}
 
+export default function TagSelectionButtonsWrapper({
+	isTag = false,
+	selectedTags,
+	onChangeTagHandler,
+}: ITagSelectionButtonsWrapperProps) {
 	return (
 		<div className={`${isTag ? 'flex gap-2' : 'grid w-96 grid-cols-3 gap-x-4 gap-y-3'}`}>
 			{tags.map((tag) => {
