90b251b477b1d6b7f68b5296c215f08c359d0e88
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-27 10:58:55 +0900

    feat: 사용자 테마 선택 기능 구현

--- src/common/components/Modals/ThemeSelectionModal.tsx
+++ src/common/components/Modals/ThemeSelectionModal.tsx
@@ -1,10 +1,26 @@
+'use client';
+
 import BaseModal from '@/common/components/Modals/BaseModal';
 import TagSelectionButtonsWrapper from '@/common/components/Buttons/TagSelectionButtonsWrapper';
+import useTagSelectionButton from '@/common/hooks/useThemeSelectionButton';
 
 export default function TagSelectionModal() {
+	const { onChangeTagHandler, selectedTags } = useTagSelectionButton();
+
+	const onClickMainButtonHandler = async () => {
+		const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/user/tag`, {
+			method: 'POST',
+			body: JSON.stringify({ tags: selectedTags }),
+		});
+		console.log(res.json());
+	};
+
 	return (
-		<BaseModal type="tagSelection">
-			<TagSelectionButtonsWrapper />
+		<BaseModal type="tagSelection" onMainButtonClick={onClickMainButtonHandler}>
+			<TagSelectionButtonsWrapper
+				selectedTags={selectedTags}
+				onChangeTagHandler={onChangeTagHandler}
+			/>
 		</BaseModal>
 	);
 }
