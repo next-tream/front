fb1928b8cbc6a9a12f6930614f4ac1475ede47be
지승 <rjs8833@gmail.com>
2024-12-27 22:36:39 +0900

    feedback: 지승이 생각이 담긴 nextauth 로직 처리하기

--- src/app/api/cookie/route.ts
+++ src/app/api/cookie/route.ts
@@ -1,5 +1,7 @@
-import { cookies } from 'next/headers';
 import { NextResponse } from 'next/server';
+import { cookies } from 'next/headers';
+
+// middleware.ts이라는 파일을 만들어서 해야할텐데..? 아마두 ? -> 제가 테스트해보겠습니다.
 
 export async function GET(request: Request) {
 	const accessToken = cookies().get('accessToken');
