f300b4ebbff44889d864fd62b6abb1fba7bf9582
yheseul <kangyheseul@naver.com>
2024-12-28 19:37:28 +0900

    feat: 회원가입 인증 모달을 인터셉트 라우팅으로 적용

---
+++ src/app/@modal/(.)kakaoAuth/page.tsx
@@ -0,0 +1,5 @@
+import KakaoAuthModal from '@/common/components/Modals/KakaoAuthModal';
+
+export default function KakaoAuthInterceptPage() {
+	return <KakaoAuthModal authenticationTime={300} />;
+}
