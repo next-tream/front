f3b43001eb4904db5314064dcc536452d939b1b4
yheseul <kangyheseul@naver.com>
2024-12-29 21:52:38 +0900

    refactor: 이메일 쿼리 스트링 개선

--- src/app/@modal/(.)kakaoAuth/page.tsx
+++ src/app/@modal/(.)kakaoAuth/page.tsx
@@ -1,5 +1,10 @@
 import KakaoAuthModal from '@/common/components/Modals/KakaoAuthModal';
 
-export default function KakaoAuthInterceptPage() {
-	return <KakaoAuthModal authenticationTime={300} />;
+export default function KakaoAuthInterceptPage({
+	searchParams,
+}: {
+	searchParams: { email?: string };
+}) {
+	const email = searchParams.email || '';
+	return <KakaoAuthModal authenticationTime={300} email={email} />;
 }
