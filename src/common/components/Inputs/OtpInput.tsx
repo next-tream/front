e176a2d5aa3204bd4d8c696ba0b0c90aa9815d53
yheseul <kangyheseul@naver.com>
2024-12-29 12:08:02 +0900

    feat: 이메일 인증 구현 완료

--- src/common/components/Inputs/OtpInput.tsx
+++ src/common/components/Inputs/OtpInput.tsx
@@ -1,8 +1,9 @@
+import { IOtpInputProps } from '@/common/types/input.interface';
 import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp';
 
-export default function OtpInput() {
+export default function OtpInput({ onChangeOtpHandle, otpCode, name }: IOtpInputProps) {
 	return (
-		<InputOTP maxLength={6}>
+		<InputOTP maxLength={6} onChange={onChangeOtpHandle} value={otpCode} name={name}>
 			<InputOTPGroup>
 				{new Array(6).fill('').map((_, index) => (
 					<InputOTPSlot key={index} index={index} className="size-14" />
--- src/common/components/Modals/KakaoAuthModal.tsx
+++ src/common/components/Modals/KakaoAuthModal.tsx
@@ -1,21 +1,44 @@
+'use client';
+
 import BaseModal from '@/common/components/Modals/BaseModal';
 import { IKakaoAuthModalProps } from '@/common/types/modal.interface';
 import { ArrowPathIcon } from '@heroicons/react/24/outline';
 import Timer from '@/common/components/Modals/Timer';
 import OtpInput from '@/common/components/Inputs/OtpInput';
+import { useFormState } from 'react-dom';
+import useOtpInput from '../Inputs/hooks/useOtpInput';
+import { submitAction } from '@/common/validation/emailAuthFormValidation';
+import { useSearchParams } from 'next/navigation';
 
 export default function KakaoAuthModal({ authenticationTime }: IKakaoAuthModalProps) {
+	const { otpCode, onChangeOtpHandle } = useOtpInput();
+	const searchParams = useSearchParams();
+	const email = searchParams.get('email') || '';
+
+	const [formData, setFormData] = useFormState(submitAction, {
+		code: otpCode,
+		email: email,
+	});
+
+	console.log(formData);
+
 	return (
-		<BaseModal type="kakaoAuth">
-			<div className="flexColCenter gap-4">
-				<div className="flexCol items-end gap-4">
-					<div className="center relative w-full">
-						<Timer time={authenticationTime} />
-						<ArrowPathIcon className="absolute right-2 top-0 size-4 text-mainBlack" />
+		<form action={setFormData}>
+			<BaseModal type="kakaoAuth">
+				<div className="flexColCenter gap-4">
+					<div className="flexCol items-end gap-4">
+						<div className="center relative w-full">
+							<Timer time={authenticationTime} />
+							<ArrowPathIcon className="absolute right-2 top-0 size-4 text-mainBlack" />
+						</div>
+						<OtpInput
+							onChangeOtpHandle={onChangeOtpHandle}
+							otpCode={otpCode}
+							name="code"
+						/>
 					</div>
-					<OtpInput />
 				</div>
-			</div>
-		</BaseModal>
+			</BaseModal>
+		</form>
 	);
 }
