c4b42f44b8b6101d0611243e9cd706a9b143a277
yheseul <kangyheseul@naver.com>
2024-12-30 20:48:46 +0900

    feat: 서버에 비밀번호 변경 요청하는 로직 구현 - 비밀번호 변경 API 호출을 위한 axios 요청 추가 - 성공 및 실패 응답 처리 로직 구현

---
+++ src/common/services/requestChangePassword.ts
@@ -0,0 +1,19 @@
+'use server';
+
+import { api } from '../configs/axios.config';
+import { IPasswordChangePrevState } from '../types/formValidation.interface';
+
+export const requestChangePassword = async ({ password, email }: IPasswordChangePrevState) => {
+	try {
+		const response = await api.post('/auth/change/password', {
+			password,
+			email,
+		});
+
+		if (response.status === 200) {
+			console.log('비밀번호 변경 완료');
+		}
+	} catch (error) {
+		alert(`비밀번호 변경 실패: ${error}`);
+	}
+};
