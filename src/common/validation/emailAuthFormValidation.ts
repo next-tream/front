54a619b28bddfd5e3c5de772a686bdd336033e80
yheseul <kangyheseul@naver.com>
2024-12-30 10:43:52 +0900

    refactor

--- src/common/validation/emailAuthFormValidation.ts
+++ src/common/validation/emailAuthFormValidation.ts
@@ -6,7 +6,8 @@ import { IEmailAuthPrevState } from '../types/formValidation.interface';
 export const submitAction = async (prevState: IEmailAuthPrevState, formData: FormData) => {
 	const code = (formData.get('code') as string) || '';
 	const email = prevState.email;
+	const isPassword = false;
 
-	emailAuthentication({ code, email });
+	emailAuthentication({ code, email, isPassword });
 	return { code, email };
 };
