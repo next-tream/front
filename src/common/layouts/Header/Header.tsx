6da652d9728c8af970c7779ad611743f27440604
h0ngsik <hongsik.choi.dev@gmail.com>
2024-12-30 15:15:21 +0900

    design: header z-index 수정

--- src/common/layouts/Header/Header.tsx
+++ src/common/layouts/Header/Header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 export default function Header() {
 	return (
-		<div className="betweenCenter sticky top-0 z-50 h-24 flex-row border-b border-solid border-darkGray bg-mainBlack py-5 pr-3.5">
+		<div className="betweenCenter sticky top-0 z-30 h-24 flex-row border-b border-solid border-darkGray bg-mainBlack py-5 pr-3.5">
 			<div className="between">
 				<Bars3Icon className="size-14" />
 				<p className="text-5xl font-semibold">Nextream</p>
