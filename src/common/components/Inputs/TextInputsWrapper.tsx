249372de3ebd98a2bbef5b2dc4d9b4c6588c249e
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-02 20:44:00 +0900

    feat: PasswordInput 컴포넌트 적용

--- src/common/components/Inputs/TextInputsWrapper.tsx
+++ src/common/components/Inputs/TextInputsWrapper.tsx
@@ -1,22 +1,34 @@
-import { ITextInputsWrapperProps } from '@/common/types/textInput.interface';
-import MODAL_CONFIG from '@/common/constants/modal.const';
 import TextInput from '@/common/components/Inputs/TextInput';
+import MODAL_CONFIG from '@/common/constants/modal.const';
+import { ITextInputsWrapperProps } from '@/common/types/textInput.interface';
+import PasswordInput from './PasswordInput';
 
 export default function TextInputsWrapper({ type, errors }: ITextInputsWrapperProps) {
 	const { inputs } = MODAL_CONFIG[type];
 
 	return (
 		<div className="flexCol gap-4">
-			{inputs.map((element, index) => (
-				<TextInput
-					name={element.name}
-					key={index}
-					type={element.type}
-					title={element.title}
-					placeholder={element.placeholder}
-					errors={errors}
-				/>
-			))}
+			{inputs.map((element, index) =>
+				element.type === 'password' ? (
+					<PasswordInput
+						name={element.name}
+						key={index}
+						type={element.type}
+						title={element.title}
+						placeholder={element.placeholder}
+						errors={errors}
+					/>
+				) : (
+					<TextInput
+						name={element.name}
+						key={index}
+						type={element.type}
+						title={element.title}
+						placeholder={element.placeholder}
+						errors={errors}
+					/>
+				),
+			)}
 		</div>
 	);
 }
