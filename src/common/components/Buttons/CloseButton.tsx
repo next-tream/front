c04be6316cf77ca9e1cbcf70ae7c4a7f8e79f3eb
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-02 16:54:58 +0900

    chore: 모달 닫기 시 이전 페이지로 이동하도록 수정

--- src/common/components/Buttons/CloseButton.tsx
+++ src/common/components/Buttons/CloseButton.tsx
@@ -5,10 +5,10 @@ import { useRouter } from 'next/navigation';
 
 export default function CloseButton() {
 	const router = useRouter();
-	
+
 	return (
 		<button
-			onClick={() => router.push('/')}
+			onClick={() => router.back()}
 			className="absolute right-4 top-4 flex size-8 items-center justify-center rounded-full bg-lightGray"
 		>
 			<XMarkIcon className="size-6 text-subBlack" />
