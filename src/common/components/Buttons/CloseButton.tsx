304ab5d44a509299e0bc57479c8ede99a9d97c7d
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-03 10:45:56 +0900

    feat: 모달 열렸을 때, 스크롤 히든 처리 구현

--- src/common/components/Buttons/CloseButton.tsx
+++ src/common/components/Buttons/CloseButton.tsx
@@ -1,14 +1,9 @@
-'use client';
-
 import { XMarkIcon } from '@heroicons/react/24/outline';
-import { useRouter } from 'next/navigation';
 
-export default function CloseButton() {
-	const router = useRouter();
-
+export default function CloseButton({ onClick }: { onClick: () => void }) {
 	return (
 		<button
-			onClick={() => router.back()}
+			onClick={onClick}
 			className="absolute right-4 top-4 flex size-8 items-center justify-center rounded-full bg-lightGray"
 		>
 			<XMarkIcon className="size-6 text-subBlack" />
--- src/common/components/Modals/BaseModal.tsx
+++ src/common/components/Modals/BaseModal.tsx
@@ -1,8 +1,13 @@
+'use client';
+
 import BaseButton from '@/common/components/Buttons/BaseButton';
 import CloseButton from '@/common/components/Buttons/CloseButton';
 import { IBaseModalProps } from '@/common/types/modal.interface';
 import MODAL_CONFIG from '@/common/constants/modal.const';
 import { splitByDotAndRenderParagraphs } from '@/common/utils/splitByDotAndRenderParagraphs';
+import { useEffect } from 'react';
+import { useToggle } from '@/common/hooks/useToggle';
+import { useRouter } from 'next/navigation';
 
 export default function BaseModal({
 	children,
@@ -10,14 +15,34 @@ export default function BaseModal({
 	onMainButtonClick,
 	onSubButtonClick,
 }: IBaseModalProps) {
+	const router = useRouter();
 	const { title, description, mainButtonLabel, subButtonLabel } = MODAL_CONFIG[type];
+	const { isToggle: isOpen, onClickToggle } = useToggle(true);
+
+	const onClickCloseHandler = () => {
+		onClickToggle();
+		if (type === 'logout') router.back();
+		else router.push('/');
+	};
+
+	useEffect(() => {
+		if (isOpen) {
+			document.body.classList.add('overflow-hidden');
+		} else {
+			document.body.classList.remove('overflow-hidden');
+		}
+
+		return () => {
+			document.body.classList.remove('overflow-hidden');
+		};
+	}, [isOpen]);
 
 	return (
 		<>
 			<div className="fixed inset-0 z-40 bg-mainBlack bg-opacity-50" />
 			<div className="center fixed inset-0 z-50">
 				<div className="flexCol relative h-1/2 min-h-[544px] w-1/3 min-w-[350px] max-w-md gap-8 rounded-2xl bg-mainWhite px-6 pb-4 pt-11">
-					<CloseButton />
+					<CloseButton onClick={onClickCloseHandler} />
 					<div className="flexColCenter gap-7">
 						<div className="text-4xl font-bold text-mainBlack">{title}</div>
 
