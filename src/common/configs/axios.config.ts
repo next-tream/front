96e30b28cbb1591df9e9219d0ecef90f45f9b305
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-03 15:44:36 +0900

    refactor: authOptions 파일 분리로 인한 import 경로 변경

--- src/common/configs/axios.config.ts
+++ src/common/configs/axios.config.ts
@@ -1,6 +1,6 @@
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 import axios, { AxiosInstance } from 'axios';
 import { getServerSession } from 'next-auth';
+import { authOptions } from './nextAuth.config';
 
 export const api: AxiosInstance = axios.create({
 	baseURL: process.env.NEXT_PUBLIC_BASE_URL,
