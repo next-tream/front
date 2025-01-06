67d3e22014ce52d6f41357e96ddd06161939bc4f
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-06 11:48:54 +0900

    design: 설정 페이지 퍼블리싱

---
+++ src/app/profile/settings/page.tsx
@@ -0,0 +1,9 @@
+import StreamSettingCard from './_components/StreamSettingCard';
+
+export default function SettingsPage() {
+	return (
+		<div className="flexCol gap-3">
+			<StreamSettingCard />
+		</div>
+	);
+}
