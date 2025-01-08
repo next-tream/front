e8cc3fc3a07ea6c412323664a094543f054da7c8
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-08 13:04:40 +0900

    feat: Refresh Token Route 구현 - 브라우저 AccessToken 만료 시 Next.js 서버에게 Refresh 요청 후 Next.js 서버에서 백엔드 서버로 Refresh Token API 호출

---
+++ src/app/api/auth/reissue/route.ts
@@ -0,0 +1,39 @@
+'use server';
+
+import { setCookie } from '@/common/utils/setCookie';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
+
+export const POST = async () => {
+	const cookiesStore = cookies();
+	const cookieHeader = cookiesStore
+		.getAll()
+		.map(({ name, value }) => `${name}=${value}`)
+		.join('; ');
+
+	console.log(cookieHeader);
+
+	const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/auth/reissue`, {
+		method: 'POST',
+		headers: {
+			Cookie: cookieHeader,
+		},
+	});
+
+	const data = await response.json();
+
+	if (!data.accessToken)
+		return NextResponse.json({
+			statusCode: `${data.statusCode}`,
+			ErrorMessage: `${data.message}`,
+			message: '토큰을 재발급하지 못했습니다.',
+		});
+
+	setCookie('accessToken', data.accessToken, {
+		httpOnly: true,
+		secure: true,
+		maxAge: 60 * 60 * 1,
+	});
+
+	return NextResponse.json({ message: '토큰이 재발급 되었습니다.' });
+};
