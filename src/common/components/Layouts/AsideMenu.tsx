535680c9a5499f5f95e1e605035f7f5219390f6f
yheseul <kangyheseul@naver.com>
2025-01-10 17:28:51 +0900

    feat: 메인 페이지 여부 및 사용자 유형(유저/스트리머)에 따라 사이드 메뉴 렌더링 분기 처리

--- src/common/components/Layouts/AsideMenu.tsx
+++ src/common/components/Layouts/AsideMenu.tsx
@@ -2,19 +2,27 @@
 
 import { usePathname } from 'next/navigation';
 import MenuItemWrapper from '@/common/components/MenuItems/MenuItemWrapper';
-import { mainMenuItems, streamerMenuItems } from '@/common/constants/menuItems.const';
+import {
+	mainMenuItems,
+	streamerMenuItems,
+	userMenuItems,
+} from '@/common/constants/menuItems.const';
 import { ASIDE_LAYOUT_PATHS } from '@/common/constants/layout.const';
+import { useSession } from 'next-auth/react';
 
 export default function AsideMenu() {
 	const path = usePathname();
+	const user = useSession();
+	const role = user?.data?.user.role;
 
 	return (
 		<div>
-			{ASIDE_LAYOUT_PATHS.includes(path) && (
+			{ASIDE_LAYOUT_PATHS.includes(path) ? (
 				<MenuItemWrapper items={mainMenuItems} isAside={true} />
-			)}
-			{path.includes('profile') && (
+			) : role ? (
 				<MenuItemWrapper items={streamerMenuItems} isAside={true} />
+			) : (
+				<MenuItemWrapper items={userMenuItems} isAside={true} />
 			)}
 		</div>
 	);
