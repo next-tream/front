cfc1a910526ad81109274bec311cc00b92e124a3
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-12 00:49:31 +0900

    feat: IMessage 타입 선언

--- src/app/profile/start-streaming/type.ts
+++ src/app/profile/start-streaming/type.ts
@@ -1,3 +1,10 @@
+export interface IMessage {
+	createdAt?: Date;
+	nickname?: string;
+	message: string;
+	color?: string;
+}
+
 export interface IRankingProps {
 	isFirst: boolean;
 	color: string;
