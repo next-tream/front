71f7d0af47a3057b9ee01e8c3c41556bdb60703c
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-12 00:55:07 +0900

    design: Streaming 페이지 퍼블리싱

--- src/app/streaming/[roomId]/page.tsx
+++ src/app/streaming/[roomId]/page.tsx
@@ -1,17 +1,23 @@
 'use client';
-
-import Chat from '@/app/profile/start-streaming/_components/Chat';
-import { useToggle } from '@/common/hooks/useToggle';
 import { useParams } from 'next/navigation';
-import React from 'react';
+import { useToggle } from '@/common/hooks/useToggle';
+import Chat from '@/app/profile/start-streaming/_components/Chat';
+import LivePlayer from '@/common/components/LivePlayer';
 
 export default function StreamingPage() {
 	const { isToggle, onClickToggle } = useToggle(true);
 	const { roomId } = useParams();
 
 	return (
-		<div>
-			<Chat roomId={roomId.toString()} isToggle={isToggle} onClickToggle={onClickToggle} />
+		<div className="w-min-[40rem] h-min-[40rem]">
+			<div className="flex h-full w-full gap-2">
+				<LivePlayer />
+				<Chat
+					roomId={roomId.toString()}
+					isToggle={isToggle}
+					onClickToggle={onClickToggle}
+				/>
+			</div>
 		</div>
 	);
 }
