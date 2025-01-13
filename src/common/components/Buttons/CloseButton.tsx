601215d8fc14a472fdb3721e0ea4bde747c3ee8f
yheseul <kangyheseul@naver.com>
2025-01-13 17:01:14 +0900

    fix: 회원가입 모달에서 Enter 키 누를 시 의도치 않게 닫히는 문제 해결 - close 버튼에 type=button 명시하여 기본 submit 동작 방지

--- src/common/components/Buttons/CloseButton.tsx
+++ src/common/components/Buttons/CloseButton.tsx
@@ -5,6 +5,7 @@ export default function CloseButton({ onClick }: { onClick: () => void }) {
 		<button
 			onClick={onClick}
 			className="absolute right-4 top-4 flex size-8 items-center justify-center rounded-full bg-lightGray"
+			type="button"
 		>
 			<XMarkIcon className="size-6 text-subBlack" />
 		</button>
