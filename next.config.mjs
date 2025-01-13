bbbc88d5bd690fd60f32b10985c1584a0fbb34a5
지승 <rjs8833@gmail.com>
2025-01-13 15:57:23 +0900

    test: build test 1

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ const nextConfig = {
 	trailingSlash: true,
 	env: {
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 		NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
 		NEXT_PUBLIC_HLS_URL: process.env.NEXT_PUBLIC_HLS_URL,
 	},
