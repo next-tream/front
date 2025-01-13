59ae643fcc2bf717d3f27d904edddcf702ae2b00
지승 <rjs8833@gmail.com>
2025-01-13 15:02:49 +0900

    test: aws amplify

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@
 const nextConfig = {
 	reactStrictMode: false,
 	trailingSlash: true,
+	env: {
+		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+		NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+		NEXT_PUBLIC_HLS_URL: process.env.NEXT_PUBLIC_HLS_URL,
+	},
 };
 
 export default nextConfig;
