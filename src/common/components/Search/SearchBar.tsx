11a244c2ff009a92952d92010076931dd47110fb
yheseul <kangyheseul@naver.com>
2025-01-14 12:00:33 +0900

    feat: 검색바에서 엔터 키로 검색 기능 추가

--- src/common/components/Search/SearchBar.tsx
+++ src/common/components/Search/SearchBar.tsx
@@ -1,20 +1,29 @@
 import { ISearchBarProps } from '@/common/types/search.interface';
 import { MagnifyingGlassIcon, XCircleIcon } from '@heroicons/react/24/outline';
 
-export default function SearchBar({ inputText, onChangeInput, onClickReset, onClickSearch }: ISearchBarProps) {
+export default function SearchBar({
+	inputText,
+	onChangeInput,
+	onClickReset,
+	onClickSearch,
+}: ISearchBarProps) {
 	return (
-		<div className="border-mainWhite between h-12 w-full rounded-2xl border px-5 py-3.5">
-			<input
-				type="text"
-				placeholder="스트리머 게임 검색"
-				value={inputText}
-				className="w-full bg-[transparent] text-base font-medium outline-none"
-				onChange={onChangeInput}
-			/>
-			<div className="flex gap-1">
-				{inputText && <XCircleIcon onClick={onClickReset} />}
-				<MagnifyingGlassIcon onClick={onClickSearch} />
+		<form>
+			<div className="between h-12 w-full rounded-2xl border border-mainWhite px-5 py-3.5">
+				<input
+					type="text"
+					placeholder="스트리머 게임 검색"
+					value={inputText}
+					className="w-full bg-[transparent] text-base font-medium outline-none"
+					onChange={onChangeInput}
+				/>
+				<div className="flex gap-1">
+					{inputText && <XCircleIcon onClick={onClickReset} />}
+					<button type="submit" onClick={onClickSearch} className="relative">
+						<MagnifyingGlassIcon className="absolute right-1 top-0 z-50 flex size-5" />
+					</button>
+				</div>
 			</div>
-		</div>
+		</form>
 	);
 }
