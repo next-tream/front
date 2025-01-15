9f03e2df958481aab696f0bbf60c59e434ca1515
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-15 15:56:33 +0900

    chore: 잘못된 병합으로 인한 코드 수정

--- src/common/components/Search/hooks/useInputText.ts
+++ src/common/components/Search/hooks/useInputText.ts
@@ -2,8 +2,6 @@
 
 import { useState } from 'react';
 import { TInputChangeEvent } from '@/common/types/handler.type';
-import { useSession } from 'next-auth/react';
-import { requestSearchHistory } from '@/common/services/requestSearchHistory';
 
 export const useInputText = () => {
 	const [inputText, setInputText] = useState('');
@@ -18,19 +16,14 @@ export const useInputText = () => {
 	};
 
 	const onClickSearch = () => {
-		if (isLogin.data) {
-			requestSearchHistory(inputText);
-			setInputText('');
-		} else {
-			const searchHistoryList = localStorage.getItem('search');
-			if (searchHistoryList) {
-				searchHistoryValues.push(...JSON.parse(searchHistoryList));
-			}
-			searchHistoryValues.push(inputText);
-			localStorage.removeItem('search');
-			localStorage.setItem('search', JSON.stringify(searchHistoryValues));
-			setInputText('');
+		const searchHistoryList = localStorage.getItem('search');
+		if (searchHistoryList) {
+			searchHistoryValues.push(...JSON.parse(searchHistoryList));
 		}
+		searchHistoryValues.push(inputText);
+		localStorage.removeItem('search');
+		localStorage.setItem('search', JSON.stringify(searchHistoryValues));
+		setInputText('');
 	};
 
 	return {
