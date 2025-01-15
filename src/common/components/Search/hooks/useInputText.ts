8ea90d8c6cc6b392740a4d4a885b88c0fbb057c0
yheseul <kangyheseul@naver.com>
2025-01-15 14:25:52 +0900

    fix: 검색 히스토리 수정

--- src/common/components/Search/hooks/useInputText.ts
+++ src/common/components/Search/hooks/useInputText.ts
@@ -1,14 +1,11 @@
 'use client';
 
-import { FormEvent, useState } from 'react';
+import { useState } from 'react';
 import { TInputChangeEvent } from '@/common/types/handler.type';
-import { useSession } from 'next-auth/react';
-import { requestSearchHistory } from '@/common/services/requestSearchHistory';
 
 export const useInputText = () => {
 	const [inputText, setInputText] = useState('');
 	const searchHistoryValues: string[] = [];
-	const isLogin = useSession();
 
 	const onChangeInput = (event: TInputChangeEvent) => {
 		setInputText(event.target.value);
@@ -19,19 +16,14 @@ export const useInputText = () => {
 	};
 
 	const onClickSearch = () => {
-		if (isLogin.data) {
-			requestSearchHistory(inputText);
-			setInputText('');
-		} else {
-			const searchHistoryList = localStorage.getItem('search');
-			if (searchHistoryList) {
-				searchHistoryValues.push(...JSON.parse(searchHistoryList));
-			}
-			searchHistoryValues.push(inputText);
-			localStorage.removeItem('search');
-			localStorage.setItem('search', JSON.stringify(searchHistoryValues));
-			setInputText('');
+		const searchHistoryList = localStorage.getItem('search');
+		if (searchHistoryList) {
+			searchHistoryValues.push(...JSON.parse(searchHistoryList));
 		}
+		searchHistoryValues.push(inputText);
+		localStorage.removeItem('search');
+		localStorage.setItem('search', JSON.stringify(searchHistoryValues));
+		setInputText('');
 	};
 
 	return {
