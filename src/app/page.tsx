a71c7eb1be8eb247b5edf62f011d63f615a62664
yheseul <kangyheseul@naver.com>
2025-01-17 02:17:18 +0900

    feat: 메인 페이지 리팩토링 목데이터 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,16 @@ import { CarouselSpacing } from '@/common/components/Carousel/CarouselSpacing';
 import MainVideo from '@/common/components/Video/MainVideo';
 import MenuContainer from '@/common/components/MenuContainer';
 import PageTitle from '@/common/components/PageTitle';
-import Video from '@/common/components/Video/Video';
+import { followingVideo, liveVideo, recommendVideo } from '@/common/constants/data';
 
 export default function Home() {
 	return (
 		<div className="flexCol h-full w-full gap-5 p-5">
-			<div className="center w-full lg:h-60 2xl:h-[650px]">
+			<div className="center w-full lg:h-80 2xl:h-[700px]">
 				<MainVideo />
 			</div>
 			<PageTitle id="recommendation" title="추천" index={0} />
-			<MenuContainer>
-				<Video showLiveStatus={true} />
-			</MenuContainer>
+			<MenuContainer data={recommendVideo} />
 			<div className="flexCol w-full gap-3 text-3xl font-semibold">
 				<PageTitle id="category" title="카테고리" index={1} />
 				<div className="w-full">
@@ -21,13 +19,9 @@ export default function Home() {
 				</div>
 			</div>
 			<PageTitle id="following" title="팔로잉" index={2} />
-			<MenuContainer>
-				<Video showLiveStatus={true} />
-			</MenuContainer>
+			<MenuContainer data={followingVideo} />
 			<PageTitle id="live" title="라이브" index={3} />
-			<MenuContainer>
-				<Video showLiveStatus={true} />
-			</MenuContainer>
+			<MenuContainer data={liveVideo} />
 		</div>
 	);
 }
