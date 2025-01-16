a0e9d29b14f7acd132eac6e15386b2879bfd0c80
yheseul <kangyheseul@naver.com>
2025-01-17 02:16:20 +0900

    TagsWrapper 목데이터 적용

--- src/common/components/Tags/TagsWrapper.tsx
+++ src/common/components/Tags/TagsWrapper.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import TagContainer from '@/common/components/Tags/TagContainer';
-import { ITagWrapperProps } from '@/common/types/tag.interface';
 
-export default function TagsWrapper({ tags }: ITagWrapperProps) {
+export default function TagsWrapper({ tags }: { tags: string }) {
+	const tagsArray = tags.split('/');
 	return (
 		<div className="flex gap-2">
-			{tags.map((element, index) => (
+			{tagsArray.map((element, index) => (
 				<TagContainer key={index} name={element} />
 			))}
 		</div>
