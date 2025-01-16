d4043575c5197c7629628a584adc36dc2de8f6e0
yheseul <kangyheseul@naver.com>
2025-01-17 02:01:54 +0900

    refactor: 스트리머 정보 컴포넌트 목데이터 적용 및 공용 애니메이션 효과 적용

--- src/common/components/StreamerInfoContainers/StreamerInfoContainer.tsx
+++ src/common/components/StreamerInfoContainers/StreamerInfoContainer.tsx
@@ -1,20 +1,25 @@
 import CircleImage from '@/common/components/CircleImage';
 import { IStreamerInfoContainerProps } from '@/common/types/container.interface';
+import { formatRoomTags } from '@/common/utils/formatRoomTags';
 
 export default function StreamerInfoContainer({
 	title,
 	name,
 	category,
 	image,
+	isLive,
 }: IStreamerInfoContainerProps) {
+	const roomTags = formatRoomTags(category);
 	return (
 		<div className="rounded-lg p-1 hover:bg-darkGray hover:bg-opacity-25">
 			<div className="flex cursor-pointer gap-2">
-				<div className="lg:size-8 2xl:size-14">{image && <CircleImage src={image} />}</div>
+				<div className="lg:size-8 2xl:size-14">
+					{image && <CircleImage src={image} isStreaming={isLive} />}
+				</div>
 				<div className="flexCol gap-0.5">
 					<p className="font-bold text-mainWhite lg:text-sm 2xl:text-2xl">{title}</p>
 					<p className="font-semibold text-mainWhite lg:text-xs 2xl:text-xl">{name}</p>
-					<p className="font-medium text-darkGray lg:text-xs 2xl:text-xl">{category}</p>
+					<p className="font-medium text-darkGray lg:text-xs 2xl:text-xl">{roomTags}</p>
 				</div>
 			</div>
 		</div>
