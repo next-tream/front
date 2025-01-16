2ac9190c36ea405a21199ea132d0480027578bd2
yheseul <kangyheseul@naver.com>
2025-01-17 08:56:29 +0900

    style: gap 사이즈 변경

--- src/common/components/MenuContainer.tsx
+++ src/common/components/MenuContainer.tsx
@@ -4,7 +4,7 @@ import Video from './Video/Video';
 export default function MenuContainer({ data }: { data: IVideo[] }) {
 	return (
 		<div className="flexCol aspect-video gap-3 text-3xl font-semibold">
-			<div className="grid h-full w-full grid-cols-3 gap-4">
+			<div className="grid h-full w-full grid-cols-3 gap-5">
 				{data.map((el, index) => (
 					<div key={index}>
 						<Video data={el} />
