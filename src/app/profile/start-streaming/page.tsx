7e67198bee3a30e83aa997991f3b8b83a3f86980
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-16 11:43:00 +0900

    design: 태그 및 인풋 별 타이틀 퍼블리싱

--- src/app/profile/start-streaming/page.tsx
+++ src/app/profile/start-streaming/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 
-import { useToggle } from '@/common/hooks/useToggle';
-
 import Chat from './_components/Chat';
 import PageTitle from '@/common/components/PageTitle';
 import StartStreamingButton from '@/app/profile/start-streaming/_components/StartStreamingButton';
 import SubInput from '@/app/profile/start-streaming/_components/SubInput';
 import LivePlayer from '@/common/components/LivePlayer';
 import useStartStreaming from './hook';
+import TextTagButtonsWrapper from '@/common/components/Buttons/TextTagButtonsWrapper';
+
+import { useToggle } from '@/common/hooks/useToggle';
 import { useStreamingStateStore } from './_store/useStreamingStateStore';
 
 export default function StartStreamingPage() {
@@ -15,10 +16,12 @@ export default function StartStreamingPage() {
 	const { isStreaming } = useStreamingStateStore();
 	const {
 		roomId,
+		selectedTags,
 		onChangeNameHandler,
 		onChangeContentHandler,
 		onClickStartStreamingButtonHandler,
 		onClickStopStreamingButtonHandler,
+		onChangeTagHandler,
 	} = useStartStreaming();
 
 	const onClickButtonHandler = isStreaming
@@ -27,16 +30,34 @@ export default function StartStreamingPage() {
 
 	return (
 		<div className="flex h-full w-full gap-2">
-			<div className={`${isToggle ? 'w-2/3' : 'w-[96%]'} flex flex-col gap-2`}>
+			<div
+				className={`${isToggle ? 'w-2/3' : 'w-full'} scrollbar-none flexCol max-h-[calc(100vh-140px)] gap-3 overflow-scroll`}
+			>
 				<PageTitle title="LIVE STREAM" />
 				<LivePlayer />
-				<SubInput placeholder="스트리밍 제목" onChange={onChangeNameHandler} />
-				<div>방송 주제 태그 선택</div>
-				<SubInput placeholder="스트리밍 설명" onChange={onChangeContentHandler} />
-				<StartStreamingButton
-					title={`스트리밍 ${isStreaming ? '종료' : '시작'}`}
-					onClick={onClickButtonHandler}
+				<div className="flex items-center gap-6">
+					<p className="text-nowrap text-xl font-bold">방송 제목</p>
+					<SubInput placeholder="제목을 입력해 주세요." onChange={onChangeNameHandler} />
+				</div>
+
+				<div className="flex items-center gap-6">
+					<p className="text-nowrap text-xl font-bold">방송 설명</p>
+					<SubInput
+						placeholder="설명을 입력해 주세요."
+						onChange={onChangeContentHandler}
+					/>
+				</div>
+				<div className="mt-3 text-nowrap text-xl font-bold">스트리밍 카테고리 선택</div>
+				<TextTagButtonsWrapper
+					selectedTags={selectedTags}
+					onChangeTagHandler={onChangeTagHandler}
 				/>
+				<div className="mt-3 h-14">
+					<StartStreamingButton
+						title={`스트리밍 ${isStreaming ? '종료' : '시작'}`}
+						onClick={onClickButtonHandler}
+					/>
+				</div>
 			</div>
 			<Chat roomId={roomId} isToggle={isToggle} onClickToggle={onClickToggle} />
 		</div>
