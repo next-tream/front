5b323227eb2b5f1653df03bd33529d56eef01b16
h0ngsik <hongsik.choi.dev@gmail.com>
2025-01-17 11:34:53 +0900

    design: 프로필 점검 페이지 수정

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,10 +2,10 @@ import Link from 'next/link';
 
 const page = () => {
 	return (
-		<div className="flexColCenter h-full w-full">
-			<div className="flexColCenter w-1/2 gap-3">
+		<div className="flexColCenter h-[80vh] w-full">
+			<div className="flexColCenter w-1/2 gap-4">
 				<p className="text-5xl">🛠️ 페이지 점검 중 🛠️</p>
-				<p>페이지 리뉴얼 중입니다. 빠른 시일내에 찾아뵙겠습니다.</p>
+				<p className="mb-6">페이지 리뉴얼 중입니다. 빠른 시일내에 찾아뵙겠습니다.</p>
 				<Link href="/" className="baseSubBtn">
 					Return Home
 				</Link>
